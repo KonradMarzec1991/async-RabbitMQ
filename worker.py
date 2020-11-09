d0059801bd693df4f7d8b79f614952b06b23118a
KonradMarzec1991 <konrimarzec@gmail.com>
2020-11-09 22:30:25 +0100

    Add work queues

--- worker.py
+++ worker.py
@@ -1,3 +1,4 @@
+import time
 import pika, sys, os
 
 
@@ -5,12 +6,16 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='task_queue', durable=True)
 
     def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+        print(" [x] Received %r" % body.decode())
+        time.sleep(body.count(b'.'))
+        print(" [x] Done")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='task_queue', on_message_callback=callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
