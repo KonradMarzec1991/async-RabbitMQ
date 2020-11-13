c14995e529c705f3c1bd2e81a75ed3ef1c93215d
KonradMarzec1991 <konrimarzec@gmail.com>
2020-11-13 16:17:41 +0100

    Create PairReceiver class

--- consumer/receiver.py
+++ consumer/receiver.py
@@ -1,34 +1,11 @@
 import os
-import pika
 import sys
-import json
-from sqlite3 import connect
-from model.pair import Pair
+from server.rabbit.rabbitFrame import PairReceiver
 
 
 def main():
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='localhost')
-    )
-    channel = connection.channel()
-    channel.queue_declare(queue='pair')
-
-    def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
-        body = json.loads(body)
-        key = body['key']
-        value = body['value']
-        with connect('/home/konrad/PycharmProjects/async_rabbitMQ/server/pair.db') as conn:
-            Pair.save(conn, key, value)
-
-    channel.basic_consume(
-        queue='pair',
-        on_message_callback=callback,
-        auto_ack=True
-    )
-
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
+    receiver = PairReceiver()
+    receiver.call()
 
 
 if __name__ == '__main__':
